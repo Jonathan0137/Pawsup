2c9e4ea0cfdf84e6935c65e0df902cd06117e0fa
Jonathan Bai <47866029+Jonathan0137@users.noreply.github.com>
2021-10-04 16:27:32 -0400

    Node 39 create footer (#6)
    * added header, init setup repo
    
    * NODE-39 added footer component
    
    * fixed classname

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,18 @@
+import Footer from '../components/Footer';
 import HeaderMenu from '../components/HeaderMenu';
 
 const HomePage = () => {
    
     
     return (
-
-        <HeaderMenu />
+        <>
+            <HeaderMenu />
+            <div className="text-center">
+                <h1>This is Home Page</h1>
+            </div>
+            <Footer/>
+        </>
+        
 
     );
 }
