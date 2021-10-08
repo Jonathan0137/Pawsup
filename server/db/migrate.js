4c247d7695bb28ce77ded5ffe202423d7171738d
TylerCReichert <90816922+TylerCReichert@users.noreply.github.com>
2021-10-08 14:00:17 -0400

    Node 27 database tables (#18)
    * added User Experience/User Interface
    
    added User Experience/User Interface
    
    * Add files via upload
    
    * Delete competition.docx
    
    * Add files via upload
    
    * Update README.md
    
    * Add files via upload
    
    * Delete PB.md.pdf
    
    * Create summary.md
    
    * Added personas.pdf to the repository
    
    * Update summary.md
    
    * Create process.md
    
    * Add Product Backlog (PB.md) (#2)
    
    * Add PB.md
    
    * Reorder User Stories by priority
    
    * Update PB.md
    
    * Update process.md
    
    * Update summary.md
    
    * Added to defining points for user stories.
    
    * updated story point system
    
    * Create RPM.md (#3)
    
    includes participants and our release goals over the 4 sprints
    
    * adding RPM.md and sprint1.md (#4)
    
    * Create RPM.md
    
    includes participants and our release goals over the 4 sprints
    
    * Create sprint1.md
    
    includes participants, sprint1 release goals, and the break down of the user stories that we will implement
    
    * Add sprint 0 marks
    
    * updated migrate
    
    added table initialization to migrate with a user id, username, password, email, and location
    
    Co-authored-by: Jonathan Bai <47866029+Jonathan0137@users.noreply.github.com>
    Co-authored-by: Mengqi-Zhao <90720023+Mengqi-Zhao@users.noreply.github.com>
    Co-authored-by: Gary Xie <garyxie13@live.com>
    Co-authored-by: Gary Xie <47544104+GaryJX@users.noreply.github.com>
    Co-authored-by: Ivan Shao <44554412+SynxYu@users.noreply.github.com>
    Co-authored-by: Jesse Zhang <81975487+Arghmage@users.noreply.github.com>
    Co-authored-by: frank-skyblue <78934074+frank-skyblue@users.noreply.github.com>
    Co-authored-by: Ethan Lam <55158216+EthanLam1@users.noreply.github.com>

--- server/db/migrate.js
+++ server/db/migrate.js
@@ -11,13 +11,20 @@ async function migrate() {
         image VARCHAR(200) NOT NULL
       );
   `);
+  await db.query(`
+      CREATE TABLE IF NOT EXISTS users (
+          uid serial PRIMARY KEY,
+          username VARCHAR(30) NOT NULL,
+          password VARCHAR(30) NOT NULL,
+          email VARCHAR(30) NOT NULL,
+          location VARCHAR(30) NOT NULL
+      );
+  `);
     console.log("Successfully finished DB migrations");
-    process.exit();
   } catch (err) {
     console.error("An error occurred while running DB migrations:");
     console.error(err);
     process.exit(1);
   }
 }
-
 migrate();
