02e1e61a714af237f886a5bd0a2f2f25f7785421
Jonathan Bai <47866029+Jonathan0137@users.noreply.github.com>
2021-10-08 18:57:25 -0400

    Revert "Node 42 featured product service list (#26)" (#27)
    This reverts commit 520cd1383d91b241895987f397978f612aeba1cc.

--- server/router/router.js
+++ server/router/router.js
@@ -1,11 +1,7 @@
-const { AuthController } = require("../controllers/AuthController");
 const { ServicesController } = require("../controllers/ServicesController");
-const { UserController } = require("../controllers/UserController");
 const express = require("express");
 const router = express.Router();
 
-router.use("/auth", AuthController);
 router.use("/services", ServicesController);
-router.use("/user", UserController);
 
 exports.router = router;
