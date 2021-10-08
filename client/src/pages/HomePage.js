b3497b7ee567c3aa6370f95cde33b3c70f42e69d
Jonathan Bai <47866029+Jonathan0137@users.noreply.github.com>
2021-10-07 22:48:24 -0400

    [Node-40] home page search (#13)
    * added header, init setup repo
    
    * NODE-39 added footer component
    
    * fixed classname
    
    * added unfinished datepicker and forms
    
    * finished HomePage Service Search
    
    * Node-40 changed back proxy
    
    * fixed bug for the the console bug
    
    * Update package.json
    
    changed proxy back to 3001

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,19 +1,33 @@
+
 import Footer from '../components/Footer';
 import HeaderMenu from '../components/HeaderMenu';
+import HomePageSearchService from '../components/HomePageSearchService';
+import './HomePage.css';
 
-const HomePage = () => {
-   
+const search = (e) =>{
     
+}
+const HomePage = () => {
+
     return (
         <>
             <HeaderMenu />
-            <div className="text-center">
-                <h1>This is Home Page</h1>
+            <div className="homePage bg-light">
+                <div className="container">
+                    <div className = "row title-container text-center">
+                        <div className = "title">
+                            <h1>Temporay Harbor Warmth Like Home</h1>
+                        </div>
+                        <div className = "subtitle"> 
+                            <h4>Best Pets Service platform, Solve your Every Worries</h4>
+                        </div>
+                    </div>
+                    <HomePageSearchService search={search}/>
+                </div>
             </div>
             <Footer/>
         </>
         
-
     );
 }
 
