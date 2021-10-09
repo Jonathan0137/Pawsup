95c41a52dc9b15d51164a1420f0f2183ed896c04
frank-skyblue <78934074+frank-skyblue@users.noreply.github.com>
2021-10-08 20:49:50 -0400

    Node 26 front end sign up form (#30)
    * added header, init setup repo
    
    * NODE-39 added footer component
    
    * fixed classname
    
    * Create package-lock.json
    
    * Update package-lock.json
    
    * added unfinished datepicker and forms
    
    * pictures for feature list
    
    * creating necessary files
    
    * basic form fields no css
    
    * added css
    
    * button and form css
    
    * working on the sign up page
    
    * [NODE-30] Install server-side dependencies and add skeleton code to allow users to log in
    
    * fixed Form.Select problem
    
    * [NODE-30] Allow users to login with passport.js and maintain session
    
    * bootstrap style
    
    * bootstrap styling for sign in page
    
    * added useState parts to sign-in form
    
    added useState parts to sign-in form and linked to sign-in page as well as sign-up page
    
    * moved header menu outside of form container
    
    * working on signinform
    
    * added feature list items
    
    * [NODE-30] Add logout functionality in backend
    
    * [NODE-30] Fix issues with serializing and deserializing user
    
    * [NODE-30] Improve error handling
    
    * [NODE-30] Undo testing changes
    
    * changed css background
    
    * fixed picture rename
    
    * finished the states of the signup form
    
    * finished user facing side and states
    
    * fixed feature-list sizing problem
    
    by using 2 rows instead of 1
    
    * Update HomePage.js
    
    * updated to connect with backend
    
    added connection using axios; removed SignInForm.js to display everything on 1 page, and renamed SignInForm.css to SigninPage.css and moved it to the pages folder
    
    * added featured services
    
    * finished user facing end of featured products and services
    
    * used an endpoint to push user information to server and refactored code
    
    * fixed package-lock.json file
    
    * merging with develop
    
    * finished sprint 1 of node26
    
    * Update package.json
    
    * Update package.json
    
    Co-authored-by: jbai <jonathanbai0317@outlook.com>
    Co-authored-by: Ivan Shao <ivanshao123@gmail.com>
    Co-authored-by: Frank <frank@pop-os.localdomain>
    Co-authored-by: Gary Xie <garyxie13@live.com>
    Co-authored-by: Ivan Shao <44554412+SynxYu@users.noreply.github.com>

--- client/src/pages/SigninPage.js
+++ client/src/pages/SigninPage.js
@@ -1,57 +1,57 @@
-import React, { useState } from 'react'
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
-import HeaderMenu from '../components/HeaderMenu';
-import './SigninPage.css'
-import axios from 'axios'
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import HeaderMenu from "../components/HeaderMenu";
+import "./SigninPage.css";
+import axios from "axios";
 
 const SigninPage = () => {
-  const [details, setDetails] = useState({email: "", password: ""});
-  const [user, setUser] = useState({email: "", password: ""});
-  const [log, setLog] = useState({pass: false});
-  
+  const [details, setDetails] = useState({ email: "", password: "" });
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [log, setLog] = useState({ pass: false });
+
   const checkLogin = async () => {
     await axios
-    .post("/api/auth/login", { username: details.email, password: details.password })
-    .then(() => 
-      setLog({pass:true})
-    )
-  }
+      .post("/api/auth/login", {
+        username: details.email,
+        password: details.password,
+      })
+      .then(() => setLog({ pass: true }));
+  };
 
-  const submitHandler = e => {
+  const submitHandler = (e) => {
     e.preventDefault();
     Login(details);
-  }
+  };
 
-  const Login = user => {
-      setUser({
-          email: user.email,
-          password: user.password
-      });
-  }
+  const Login = (user) => {
+    setUser({
+      email: user.email,
+      password: user.password,
+    });
+  };
 
   const Logout = async () => {
     await axios
-    .post("/api/auth/logout")
-    .then(() =>
-      setLog({pass:false}),
-      setUser({email: "", password: ""}),
-      setDetails({email: "", password: ""})
-    )
-  }
+      .post("/api/auth/logout")
+      .then(
+        () => setLog({ pass: false }),
+        setUser({ email: "", password: "" }),
+        setDetails({ email: "", password: "" })
+      );
+  };
 
   return (
     <div className="page">
-      {(log.pass) ? (
+      {log.pass ? (
         <Container>
           <HeaderMenu />
           <h3 className="mb-4">Welcome {user.email} </h3>
-          <Button 
-            className="mb-2" 
-            variant="primary" 
-            type="submit" 
+          <Button
+            className="mb-2"
+            variant="primary"
+            type="submit"
             size="lg"
-            onClick={Logout}
-            >
+            onClick={Logout}>
             Logout
           </Button>
         </Container>
@@ -59,46 +59,63 @@ const SigninPage = () => {
         <div>
           <HeaderMenu />
           <Container fluid>
-              <Form onSubmit={submitHandler}>
+            <Form onSubmit={submitHandler}>
               <Row md>
-                  <Col>
-                  <Form.Group className="mb-3 form-signin" controlId="formEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control 
-                      type="email" 
+                <Col>
+                  <Form.Group
+                    className="mb-3 form-signin"
+                    controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type="email"
                       placeholder="Enter Email"
-                      onChange={e => setDetails({...details, email: e.target.value})} 
+                      onChange={(e) =>
+                        setDetails({ ...details, email: e.target.value })
+                      }
                       value={details.email}
-                      />
+                    />
                   </Form.Group>
-                  </Col>
-                  <Form.Group className="mb-3 form-signin" controlId="formPassword">
+                </Col>
+                <Form.Group
+                  className="mb-3 form-signin"
+                  controlId="formPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control 
-                      type="password" 
-                      placeholder="Enter Password"
-                      onChange={e => setDetails({...details, password: e.target.value})} 
-                      value={details.password}
-                      />
-                  </Form.Group>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter Password"
+                    onChange={(e) =>
+                      setDetails({ ...details, password: e.target.value })
+                    }
+                    value={details.password}
+                  />
+                </Form.Group>
               </Row>
               <Row>
-                  <Button className="mb-2" variant="primary" type="submit" size="lg" onClick={checkLogin}>
+                <Button
+                  className="mb-2"
+                  variant="primary"
+                  type="submit"
+                  size="lg"
+                  onClick={checkLogin}>
                   Login
-                  </Button>
+                </Button>
               </Row>
               <Row>
-                  <Form.Text className="text-black form-control-sm">Don't have an account yet? Sign up!</Form.Text>
+                <Form.Text className="text-black form-control-sm">
+                  Don't have an account yet? Sign up!
+                </Form.Text>
               </Row>
               <Row>
-                  <a href="/signup" className="btn btn-dark" role="button">Sign up</a>
+                <a href="/signup" className="btn btn-dark" role="button">
+                  Sign up
+                </a>
               </Row>
-              </Form>
-          </Container>    
-        </div> 
+            </Form>
+          </Container>
+        </div>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default SigninPage;
