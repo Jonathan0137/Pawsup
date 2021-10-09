dcc53bc9261e226a219c841a1dfff9d62fde2d83
Frank <frank@pop-os.localdomain>
2021-10-08 20:18:12 -0400

    Revert "finished sprint 1 for NODE26"
    This reverts commit 1226a6ff9f3722148d7d46cf6e6759e350513c06.

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import HeaderMenu from "../components/HeaderMenu";
 import { Form, Button, Row, Col } from "react-bootstrap";
-import "./SignupPage.css";
 import axios from "axios";
 
 const SignupPage = () => {
@@ -35,83 +34,81 @@ const SignupPage = () => {
   return (
     <div>
       <HeaderMenu />
-      <div className="formStyle">
-        <Row>
-          <Col>
-            <h2 className="mb-4 mt-3">Create An Account</h2>
-          </Col>
-        </Row>
-        <Form>
-          <Form.Group className="mb-2">
-            <Form.Label>Username *</Form.Label>
-            <Form.Control
-              name="username"
-              type="text"
-              value={username}
-              placeholder="Enter username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </Form.Group>
+      <Row>
+        <Col>
+          <h2 className="mb-4 mt-3">Create An Account</h2>
+        </Col>
+      </Row>
+      <Form>
+        <Form.Group className="mb-2">
+          <Form.Label>Username *</Form.Label>
+          <Form.Control
+            name="username"
+            type="text"
+            value={username}
+            placeholder="Enter username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
 
-          <Form.Group className="mb-2">
-            <Form.Label>Password *</Form.Label>
-            <Form.Control
-              name="password"
-              type="text"
-              value={password}
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Password *</Form.Label>
+          <Form.Control
+            name="password"
+            type="text"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
 
-          <Form.Group className="mb-2">
-            <Form.Label>Email address *</Form.Label>
-            <Form.Control
-              name="email"
-              type="email"
-              value={email}
-              placeholder="Enter email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Email address *</Form.Label>
+          <Form.Control
+            name="email"
+            type="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
 
-          <Form.Group className="mb-2">
-            <Form.Label>First Name *</Form.Label>
-            <Form.Control
-              type="text"
-              value={fname}
-              placeholder="First Name"
-              onChange={(e) => setFname(e.target.value)}
-            />
-          </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>First Name *</Form.Label>
+          <Form.Control
+            type="text"
+            value={fname}
+            placeholder="First Name"
+            onChange={(e) => setFname(e.target.value)}
+          />
+        </Form.Group>
 
-          <Form.Group className="mb-2">
-            <Form.Label>Last Name *</Form.Label>
-            <Form.Control
-              type="text"
-              value={lname}
-              placeholder="Last Name"
-              onChange={(e) => setLname(e.target.value)}
-            />
-          </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Last Name *</Form.Label>
+          <Form.Control
+            type="text"
+            value={lname}
+            placeholder="Last Name"
+            onChange={(e) => setLname(e.target.value)}
+          />
+        </Form.Group>
 
-          <Form.Group className="mb-3">
-            <Form.Label>City *</Form.Label>
-            <Form.Control
-              type="text"
-              value={city}
-              placeholder="City"
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>City *</Form.Label>
+          <Form.Control
+            type="text"
+            value={city}
+            placeholder="City"
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </Form.Group>
 
-          <div className="d-grid gap-2 mb-2">
-            <Button variant="primary" type="submit" onSubmit={SignUp}>
-              SignUp
-            </Button>
-          </div>
-        </Form>
-      </div>
+        <div className="d-grid gap-2 mb-2">
+          <Button variant="primary" type="submit" onSubmit={SignUp}>
+            SignUp
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
